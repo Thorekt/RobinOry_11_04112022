b10168f31d55fde904d3efc6d33ec0b4eaf76d43
Thorekt <robinory77@gmail.com>
2022-11-04 01:55:06 +0100

    routing fonctionnel et retrait de unused

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+
+function Footer() {
+
+return (
+
+<footer>
+
+<p>© 2020 by The Company</p>
+
+</footer>
+
+);
+
+}
+
+export default Footer;
---
+++ src/pages/HousingSheet.jsx
@@ -0,0 +1,9 @@
+function HoussingSheet(){
+    return (
+        <div>
+            <h1>HoussingSheet</h1>
+        </div>
+    );
+}
+
+export default HoussingSheet;
