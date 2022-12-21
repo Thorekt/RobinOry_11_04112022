c5ba47f7f79438f7dd0d4a2b58852beffe412439
Thorekt <robinory77@gmail.com>
2022-12-22 00:39:06 +0100

    Ajout du texte manquant dans le about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,18 +1,30 @@
 import React from "react";
 import Banner from "../components/Banner";
-import bannerPng  from "../assets/kalen-emsley-Bkci_8qcdvQ-unsplash 2.png";
+import bannerPng from "../assets/kalen-emsley-Bkci_8qcdvQ-unsplash 2.png";
 import Collapse from "../components/Collapse";
-import '../styles/About.css';
+import "../styles/About.css";
 
 function About() {
   return (
     <React.Fragment>
-      <Banner imgSrc={bannerPng} imgAlt='A propos'/>
+      <Banner imgSrc={bannerPng} imgAlt="A propos" />
       <div className="about">
-        <Collapse name="Fiabilité" content="Nous sommes fiable Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation"/>
-        <Collapse name="Respect" content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."/>
-        <Collapse name="Service" content="Nous offrons un service Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis "/>
-        <Collapse name="Responsabilité" content="Nous sommes responsable Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"/>
+        <Collapse
+          name="Fiabilité"
+          content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
+        />
+        <Collapse
+          name="Respect"
+          content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+        />
+        <Collapse
+          name="Service"
+          content="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+        />
+        <Collapse
+          name="Responsabilité"
+          content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+        />
       </div>
     </React.Fragment>
   );
