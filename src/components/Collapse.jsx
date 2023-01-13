6b0f99a4c6c83b117140d4224cf8c598d74b7d41
Thorekt <robinory77@gmail.com>
2023-01-13 11:30:55 +0100

    indentation

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -3,29 +3,31 @@ import '../styles/Collapse.css';
 import arrowUp from '../assets/arrow-up.png';
 import arrowDown from '../assets/arrow-down.png';
 
-function Collapse({name, content}){
-    const [isCollapsed, setIsCollapsed] = useState(true);
+function Collapse({ name, content }) {
+  const [isCollapsed, setIsCollapsed] = useState(true);
 
-return isCollapsed ? (
-    <div className="collapse">
-        <div className='name' onClick={() => setIsCollapsed(false)}>
-            <span>{name}</span>
-            <img src={arrowDown} alt="arrow-down" />
-        </div>
+  return isCollapsed ? (
+    <div className='collapse'>
+      <div className='name' onClick={() => setIsCollapsed(false)}>
+        <span>{name}</span>
+        <img src={arrowDown} alt='arrow-down' />
+      </div>
     </div>
-) : (
-    <div className="collapse open">
-        <div className='name' onClick={() => setIsCollapsed(true)}>
-            <span>{name}</span>
-            <img src={arrowUp} alt="arrow-up" />
-        </div>
-        <div className='content'>
-            {typeof(content) === 'string' ? <p>{content}</p> : content.map((item, index) => <p key={index}>{item}</p>)}
-        </div>
+  ) : (
+    <div className='collapse open'>
+      <div className='name' onClick={() => setIsCollapsed(true)}>
+        <span>{name}</span>
+        <img src={arrowUp} alt='arrow-up' />
+      </div>
+      <div className='content'>
+        {typeof content === 'string' ? (
+          <p>{content}</p>
+        ) : (
+          content.map((item, index) => <p key={index}>{item}</p>)
+        )}
+      </div>
     </div>
-);
-
-
+  );
 }
 
 export default Collapse;
