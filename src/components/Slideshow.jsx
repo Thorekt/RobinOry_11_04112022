65e06deb54707f1ec5b0afe58cb801def4fce880
Thorekt <robinory77@gmail.com>
2023-01-24 16:31:23 +0100

    compteur gallerie

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -1,31 +1,47 @@
 import arrowLeft from '../assets/arrow-left.png';
 import arrowRight from '../assets/arrow-right.png';
-import {useState} from 'react';
+import { useState } from 'react';
 import '../styles/Slideshow.css';
+import React from 'react';
 
+function Slideshow({ pictures }) {
+  const [currentPicture, setCurrentPicture] = useState(0);
 
-function Slideshow({pictures}){
-    const [currentPicture, setCurrentPicture] = useState(0);
+  if (currentPicture < 0) {
+    setCurrentPicture(pictures.length - 1);
+  }
+  if (currentPicture >= pictures.length) {
+    setCurrentPicture(0);
+  }
 
-    if(currentPicture < 0){
-        setCurrentPicture(pictures.length - 1);
-    }
-    if(currentPicture >= pictures.length){
-        setCurrentPicture(0);
-    }
-
-    return(
-        <div className="slideshow">
-                {pictures.length > 1 &&
-                <div className="control">
-                  <img src={arrowLeft} alt="arrow-left" onClick={() => setCurrentPicture(currentPicture - 1)}/>
-                  <img src={arrowRight} alt="arrow-right" onClick={() => setCurrentPicture(currentPicture + 1)}/>
-                  </div>
-                }
-            <img className="slide" src={pictures[currentPicture]} alt="Interieur du logement"/>
-        </div>
-    );
-
+  return (
+    <div className='slideshow'>
+      {pictures.length > 1 && (
+        <React.Fragment>
+          <div className='control'>
+            <img
+              src={arrowLeft}
+              alt='arrow-left'
+              onClick={() => setCurrentPicture(currentPicture - 1)}
+            />
+            <img
+              src={arrowRight}
+              alt='arrow-right'
+              onClick={() => setCurrentPicture(currentPicture + 1)}
+            />
+          </div>
+          <span className='count'>
+            {currentPicture + 1}/{pictures.length + 1}
+          </span>
+        </React.Fragment>
+      )}
+      <img
+        className='slide'
+        src={pictures[currentPicture]}
+        alt='Interieur du logement'
+      />
+    </div>
+  );
 }
 
 export default Slideshow;
